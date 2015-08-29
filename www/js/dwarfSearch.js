712c20169349206ea6041a9c440cba792869f8e0
Martin Štekl <martin.stekl@gmail.com>
2015-08-29 18:11:40 +0200

    Implemented link to line screenplay on search results

--- www/js/dwarfSearch.js
+++ www/js/dwarfSearch.js
@@ -1,5 +1,21 @@
 (function ($) {
-	$(function () {
+	var scrollTo = function (link) {
+		link = $(link);
+		$('body').animate({
+			'scrollTop': link.offset().top - 200
+		}, 10, 'swing', function () {
+			$('.screenplay-line-link').removeClass('list-group-item-info');
+			link.addClass('list-group-item-info');
+		});
+	};
 
+	$(document).on('click', '.screenplay-line-link', function () {
+		scrollTo(this);
 	});
+
+	setTimeout(function () {
+		if (location.hash && location.hash.match(/^#screenplay-line-/)) {
+			scrollTo($(location.hash));
+		}
+	}, 10);
 })(jQuery);
